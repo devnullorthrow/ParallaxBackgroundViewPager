bf2e7fad585152f038c7120e1f26ec6d86abf827
Dmitry Donskoy <donskdmitry@mail.ru>
2019-10-17 11:19:40 +0300

    Replaced heavy calls of 2 lists' comparision for light one with single loop

--- parallaxbackgroundviewpagerlibrary/src/main/java/com/yoksnod/parallaxbackgroundviewpager/ParallaxBackgroundPageListener.java
+++ parallaxbackgroundviewpagerlibrary/src/main/java/com/yoksnod/parallaxbackgroundviewpager/ParallaxBackgroundPageListener.java
@@ -10,8 +10,6 @@ import android.widget.ImageView;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-import static android.support.v4.view.ViewPager.SCROLL_STATE_IDLE;
-
 public class ParallaxBackgroundPageListener implements ViewPager.OnPageChangeListener {
 
     private static final String ANDROID_SWITCHER_TAG_SEGMENT = "android:switcher:";
@@ -44,6 +42,10 @@ public class ParallaxBackgroundPageListener implements ViewPager.OnPageChangeLis
         this.items = items;
     }
 
+    private static String makePagerFragmentTag(int index, int pagerId) {
+        return ANDROID_SWITCHER_TAG_SEGMENT + pagerId + SEPARATOR_TAG_SEGMENT + index;
+    }
+
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
         recalculateScrollDirection(positionOffset, positionOffsetPixels);
@@ -53,13 +55,8 @@ public class ParallaxBackgroundPageListener implements ViewPager.OnPageChangeLis
             return;
         }
 
-        int childrenCount = pager.getChildCount();
         int animatedItemIndex = isScrollToRight ? currentPagePosition : currentPagePosition - 1;
-        for (int index = 0; index < childrenCount; index++) {
-            View child = pager.getChildAt(index);
-            ViewPager.LayoutParams lp = (ViewPager.LayoutParams) child.getLayoutParams();
-            applyCurrentTransformPosition(lp, child, scrollX, animatedItemIndex);
-        }
+        setAlpha(animatedItemIndex, scrollX);
 
         if (isLeftEdge(scrollX)) {
             restoreInitialAlphaValues();
@@ -70,38 +67,24 @@ public class ParallaxBackgroundPageListener implements ViewPager.OnPageChangeLis
         return isLeftEdge(scrollX) && !isScrollToRight;
     }
 
-    private void applyCurrentTransformPosition(ViewPager.LayoutParams lp,
-                                               View child,
-                                               int scrollX,
-                                               int animatedItemIndex) {
+    private void setAlpha(int animatedItemIndex, int scrollX) {
+        View child = findFragmentViewByIndex(animatedItemIndex);
+        ViewPager.LayoutParams lp = (ViewPager.LayoutParams) child.getLayoutParams();
         if (lp.isDecor) {
             return;
         }
-
         float transformPos = (float) (child.getLeft() - scrollX) / (float) getClientWidth();
-        int fragIndex = findFragmentIndexByView(child);
-        if (fragIndex == animatedItemIndex) {
-            initCurrentAlpha(transformPos, fragIndex);
-        }
+        initCurrentAlpha(transformPos, animatedItemIndex);
     }
 
+    private View findFragmentViewByIndex(int index) {
+        String tag = makePagerFragmentTag(index, pager.getId());
+        Fragment page = activity.getSupportFragmentManager().findFragmentByTag(tag);
+        if (page == null)
+            throw new NoSuchElementException("no such element for tag  : " + tag);
 
-    private int findFragmentIndexByView(View currentView) {
-        int count = adapter.getCount();
-        for (int i = 0; i < count; i++) {
-            String tag = ANDROID_SWITCHER_TAG_SEGMENT + R.id.pager + SEPARATOR_TAG_SEGMENT + i;
-            Fragment page = activity.getSupportFragmentManager().findFragmentByTag(tag);
-            if (page == null)
-                throw new NoSuchElementException("no such element for tag  : " + tag);
-
-            if (page.getView() == currentView) {
-                return i;
-            }
-
-        }
-        throw new IllegalArgumentException(String.valueOf(currentView));
+        return page.getView();
     }
-
 
     private boolean isLeftEdge(int scrollX) {
         return scrollX == 0;
@@ -109,7 +92,7 @@ public class ParallaxBackgroundPageListener implements ViewPager.OnPageChangeLis
 
     private void initCurrentAlpha(float transformPos, int itemIndex) {
         ImageView currentImage = activity.findViewById(items.get(itemIndex));
-        if ((transformPos <= MINUS_INFINITY_EDGE || transformPos >= PLUS_INFINITY_EDGE)) {
+        if (transformPos <= MINUS_INFINITY_EDGE || transformPos >= PLUS_INFINITY_EDGE) {
             currentImage.setAlpha(ALPHA_TRANSPARENT);
         } else if (transformPos == SETTLED_PAGE_POSITION) {
             currentImage.setAlpha(ALPHA_OPAQUE);
@@ -125,7 +108,8 @@ public class ParallaxBackgroundPageListener implements ViewPager.OnPageChangeLis
 
     private void restoreInitialAlphaValues() {
         for (int j = items.size() - 1; j >= 0; j--) {
-            activity.findViewById(items.get(j)).setAlpha(ALPHA_OPAQUE);
+            View view = activity.findViewById(items.get(j));
+            view.setAlpha(ALPHA_OPAQUE);
         }
     }
 
@@ -144,14 +128,14 @@ public class ParallaxBackgroundPageListener implements ViewPager.OnPageChangeLis
     @Override
     public void onPageSelected(int position) {
         if (position == 0) {
-            onPageScrollStateChanged(SCROLL_STATE_IDLE);
+            onPageScrollStateChanged(ViewPager.SCROLL_STATE_IDLE);
         }
     }
 
     @Override
     public void onPageScrollStateChanged(int state) {
         currentPageScrollState = state;
-        if (state == SCROLL_STATE_IDLE) {
+        if (state == ViewPager.SCROLL_STATE_IDLE) {
             currentPagePosition = pager.getCurrentItem();
             isScrollToRight = true;
         }
